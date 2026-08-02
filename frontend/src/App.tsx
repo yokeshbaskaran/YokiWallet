8c9522fbdef3854a978986745c2d9cf96c8f626e
Yokesh <yokeshbaskaran@gmail.com>
2026-08-02 15:16:21 +0530

    added dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h2 className="p-2 text-xl text-blue-600">App is here</h2>
+    </div>
+  );
 };
 
 export default App;
