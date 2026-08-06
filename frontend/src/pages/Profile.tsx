91d70ef6e48aa9c2dc6c314540106a379aa64c3c
Yokesh <yokeshbaskaran@gmail.com>
2026-08-06 09:06:13 +0530

    profile starts editing

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -4,14 +4,29 @@ const Profile = () => {
   const { handleLogout } = useAppContext();
 
   return (
-    <div>
+    <section className="yoki min-h-screen mb-8 flex flex-col items-center gap-1">
+      {/* 1. Header  */}
+
+      <div className="my-3 flex flex-col items-center gap-1">
+        <h2>Yokesh is here</h2>
+        <div>
+          <img
+            src="/profile.png"
+            alt="my-profile"
+            width={200}
+            height={200}
+            className="w-50 yoki p-1 rounded-full"
+          />
+        </div>
+      </div>
+
       <button
         onClick={handleLogout}
-        className="p-2 text-white bg-red-700 rounded-md"
+        className="px-5 py-2 text-white bg-red-700 rounded-md cursor-pointer"
       >
         Logout
       </button>
-    </div>
+    </section>
   );
 };
 
