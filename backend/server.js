f7cec7123394c26e3e2ed128ba95635703c4931f
Yokesh <yokeshbaskaran@gmail.com>
2026-08-07 22:12:53 +0530

    app server file modified

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
+import express from "express";
+import cors from "cors";
+import transactionRoutes from "./routes/transactionRoutes.js";
+import balanceRoutes from "./routes/balanceRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import app from "./app.js";
 
 dotenv.config();
 
+const app = express();
 connectDB();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://yoki-wallet.vercel.app"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use("/api", transactionRoutes);
+app.use("/api/balance", balanceRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
