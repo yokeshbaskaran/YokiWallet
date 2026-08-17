443ab13fce7869bd9c3e7d8542e08265861b09f6
Yokesh <yokeshbaskaran@gmail.com>
2026-08-17 22:28:43 +0530

    Nav modified and updated

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -3,21 +3,21 @@ import { RiMoneyRupeeCircleFill } from "react-icons/ri";
 import { LuUserRound } from "react-icons/lu";
 import { Link } from "react-router-dom";
 import { GrTransaction } from "react-icons/gr";
-import { GoOrganization } from "react-icons/go";
+import { BsListStars } from "react-icons/bs";
 
 export const MobileBottomNav = () => {
   return (
     <>
       <section className="relative p-2 flex items-center justify-between bg-bg border-t-2 border-border h-17">
         <Link to="/">
-          <div className="flex flex-col items-center text-sm font-medium cursor-pointer">
+          <div className="px-2 flex flex-col items-center gap-1 text-xs font-medium cursor-pointer">
             <IoHomeOutline size={25} />
             <h2>Home</h2>
           </div>
         </Link>
 
         <Link to="/transactions">
-          <div className="flex flex-col items-center text-sm font-medium cursor-pointer">
+          <div className="px-2 flex flex-col items-center gap-1 text-xs font-medium cursor-pointer">
             <GrTransaction size={25} />
             <h2>Transactions</h2>
           </div>
@@ -25,24 +25,24 @@ export const MobileBottomNav = () => {
 
         <Link to="/expenses">
           {/* Floating Button */}
-          <div className="absolute left-1/2 -translate-x-1/2 -top-7 flex flex-col items-center">
-            <div className="rounded-full bg-white text-[#558B2F] flex items-center justify-center shadow-lg cursor-pointer">
-              <RiMoneyRupeeCircleFill size={55} />
+          <div className="mx-1 px-4 absolute left-1/2 -translate-x-1/2 -top-7 flex flex-col items-center">
+            <div className="rounded-full bg-white text-[#558B2F] flex items-center justify-center border border-border-strong shadow-lg cursor-pointer">
+              <RiMoneyRupeeCircleFill size={50} />
             </div>
 
-            <h2 className="mt-2 text-sm font-medium">Add expenses</h2>
+            <h2 className="mt-2 text-sm font-semibold">Add expenses</h2>
           </div>
         </Link>
 
-        <Link to="/work">
-          <div className="flex flex-col items-center text-sm font-medium cursor-pointer">
-            <GoOrganization size={25} />
-            <h2>Work</h2>
+        <Link to="/goals">
+          <div className="px-2 flex flex-col items-center gap-1 text-xs font-medium cursor-pointer">
+            <BsListStars size={25} />
+            <h2>Goals</h2>
           </div>
         </Link>
 
         <Link to="/profile">
-          <div className="flex flex-col items-center text-sm font-medium cursor-pointer">
+          <div className="px-2 flex flex-col items-center gap-1 text-xs font-medium cursor-pointer">
             <LuUserRound size={25} />
             <h2>Profile</h2>
           </div>
